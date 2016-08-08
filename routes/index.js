fc9936dc5c779bbda983a8d1bb274b4bab4e522d
Firstiar Noorwinanto <firstiar@outlook.com>
2016-08-08 18:30:38 +0700

    Moving to pogobuf api to comply with Promise.

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,18 @@ var express = require('express');
 var router = express.Router();
 var randtoken = require('rand-token');
 
+// settings must be var because we inject second layer setting item on the fly
+const fs = require('fs');
+const Settings = JSON.parse(fs.readFileSync(__dirname + '/../bot/settings.json', 'utf8'));
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     var token = randtoken.generate(32)
     req.session.token = token;
     res.render('index', {
         token: token,
-        character: req.bot.character
+        character: req.bot.character,
+        loopInterval: Settings.loopInterval
     });
 });
 
